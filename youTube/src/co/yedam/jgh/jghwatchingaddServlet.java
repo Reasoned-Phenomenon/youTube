643cb2412a2d754a65d89e453053e4343dc0b737
admin <admin@YD01-11>
2021-10-20 19:16:05 +0900

    오타 수정

--- youTube/src/co/yedam/jgh/jghwatchingaddServlet.java
+++ youTube/src/co/yedam/jgh/jghwatchingaddServlet.java
@@ -32,7 +32,7 @@ public class jghwatchingaddServlet extends HttpServlet {
       response.setContentType("text/html; charset=UTF-8");
       
       PrintWriter out = response.getWriter();
-      Gson gson = new GsonBuilder().create();
+      Gson gson = new GsonBuilder().create(); 
       
       jghwatchingDAO dao = new jghwatchingDAO();
       
